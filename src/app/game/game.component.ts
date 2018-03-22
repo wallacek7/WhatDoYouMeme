3c4947843561f6ac427182e61da8a1f64201005c
Kayla <kaylaawallace7@gmail.com>
2018-03-22 09:00:29 -0400

    components

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+
+  Model = new Game();
 
   constructor() { }
 
---
+++ src/app/models/game.ts
@@ -0,0 +1,26 @@
+
+
+
+export class Game{
+    MyQuotes: string[] = [
+        "Great minds think alike, but ...",
+        "Axis of Evil",
+        "There are two extremes"
+    ];
+    
+    Players: User[] = [];
+
+    Picture: string = 'https://media3.s-nbcnews.com/j/msnbc/components/video/201803/tdy_news_welker_trump_180315_1920x1080.nbcnews-ux-1240-700.jpg';
+
+    PlayedQuotes: Quote[] = [];
+}
+
+export class User {
+    Name: string;
+}
+
+export class Quote {
+    Text: string;
+    PlayerName: string;
+    Chosen: boolean = false;
+}
